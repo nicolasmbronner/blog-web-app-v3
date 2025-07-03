104df46f16b82b06fee83146bbb4b8884e2b440e
nicolasmbronner <nicolasmbronner@gmail.com>
2025-07-03 08:51:57 +0200

    Initial Commit
    - Repository Created
    - Project structure and files created
    - Routes commented in index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * ROUTES--------------------------------------------
+ * 
+ * GET /                      -> index page
+ * GET /articles/:id          -> read article
+ * GET /form/new              -> new article form
+ * GET /form/:id              -> edit article form
+ * GET /articles/:id/status   -> check article status
+ * 
+ * POST /articles             -> create article
+ * POST /articles/:id         -> update article
+ * 
+ * DELETE /articles/:id       -> delete article
+ */
