c7b22310e459e666b41e84b962afe73c475a282e
nicolasmbronner <nicolasmbronner@gmail.com>
2025-07-24 12:22:54 +0200

    checkpoint: ready to add article creation feature

--- articles.js
+++ articles.js
@@ -44,9 +44,3 @@ export function createArticle(title, content) {
 export function deleteArticle(id) {
     
 }
-
-// === TEST FUNCTION - À SUPPRIMER APRÈS TEST GITHUB WORKFLOW ===
-export function testBranchFunction() {
-    console.log('🧪 Test branch workflow - cette fonction sera supprimée après le merge');
-    return 'Test réussi !';
-}
